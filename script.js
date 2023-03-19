1cb28182d18f93187bf867e121e5731dca5a3dd8
goicochea iñaki <goicoina@gmail.com>
2023-03-19 11:29:12 -0300

    404FormularioDeReporte

---
+++ script.js
@@ -0,0 +1,11 @@
+ btnReportar = document.getElementById('reportar');
+
+btnReportar.addEventListener ("click", () => {
+window.open ("./ContactForm/formulario.html");
+}); 
+
+btnInicio = document.getElementById('home');
+
+btnInicio.addEventListener ("click", () => {
+window.open ("https://www.google.com/");
+});
